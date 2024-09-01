e54e065c8875a49f24e0b88d3d1d851128ab7d9f
Venkat Pavan Adithya <142378954+PavanAditya113@users.noreply.github.com>
2024-09-01 23:37:05 +0530

    Update Workdata.js

--- src/Components/Workdata.js
+++ src/Components/Workdata.js
@@ -4,22 +4,15 @@ import pro3 from "../assets/project3.png"
 const ProjectCard=[
    {
     imgsrc:pro1,
-    title: "Ecommerce Website Design",
-    text:"Thriving Facebook community annd top-ranked podcast,Amy inspires an grounded ,tangible and self -affirming sense of Wow relly can d this over 100,000 online enterpreneurs .Best selling marketing courses ",
-    view:"url.com"
+    title: "Full Stack Website Design",
+    text:"Real time messaging app ",
+    view:"https://stunning-madeleine-57de3f.netlify.app/"
    },
    {
     imgsrc:pro2,
-    title: "Chat app Design ",
-    text:"Thriving Facebook community annd top-ranked podcast,Amy inspires an grounded ,tangible and self -affirming sense of Wow relly can d this over 100,000 online enterpreneurs .Best selling marketing courses ",
-    view:"url.com"
-   },
-   {
-    imgsrc:pro3,
-    title: "Weather API",
-    text:"Thriving Facebook community annd top-ranked podcast,Amy inspires an grounded ,tangible and self -affirming sense of Wow relly can d this over 100,000 online enterpreneurs .Best selling marketing courses ",
-    view:"url.com"
+    title: "Food Delivery App",
+    text:"Full stack website designed for food delivery ",
+    view:"https://foodies83.netlify.app/"
    }
-
 ];
 export default ProjectCard;
